5680badd4525aeed897879b5cedfecfb1de7b2d0
tyrantclam <joshua.tower@gmail.com>
2021-08-19 13:30:37 -0500

    Updated to modern pypi format

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='AYBRequests',
-    url='https://github.com/jladan/package_demo',
-    author='Joshua Tower',
-    author_email='joshua.tower@gmail.com',
-    # Needed to actually package something
-    packages=['aybrequests'],
-    # Needed for dependencies
-    install_requires=['requests'],
-    # *strongly* suggested for sharing
-    version='0.2.2',
-    # The license can be anything you like
-    license='MIT',
-    description='Wrapper for requests package that includes base settings such as base url',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
-)
+if __name__ == "__main__":
+    setuptools.setup()
